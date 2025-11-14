2877209f089886367113ad378d21b446b3f2f199
ahmed692935 <ahmed692935@gmail.com>
2025-11-15 02:11:25 +0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
       <Toaster position="top-right" reverseOrder={false} />
     </>
-  )
+  );
 }
 
 export default App;
